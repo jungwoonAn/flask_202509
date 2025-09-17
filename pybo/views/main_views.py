9b503a69b016abc688820a06516c993804b36886
jungwoonAn <red0808@naver.com>
2025-09-17 18:10:58 +0900

    20250917

---
+++ pybo/views/main_views.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+
+# 블루 프린트 : 라우팅 함수를 관리하는 역할
+bp = Blueprint('main', __name__, url_prefix='/')
+
+@bp.route('/')
+def hello_pybo():
+    return 'Hello Pybo!'
+
+@bp.route('/hello')
+def hello_world():
+    return 'Hello World~!'
