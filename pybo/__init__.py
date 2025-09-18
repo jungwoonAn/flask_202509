2d8074a6544025561548cefa352eaaa647fb4093
jungwoonAn <red0808@naver.com>
2025-09-19 08:52:36 +0900

    20250919

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+# database 등록
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():  # 애플리케이션 팩토리
     app = Flask(__name__)
+    app.config.from_object(config)
+
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from . import models
 
     # 블루 프린트를 등록
     from .views import main_views
--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,12 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+from pybo.models import Question
 
 # 블루 프린트 : 라우팅 함수를 관리하는 역할
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
-def hello_pybo():
-    return 'Hello Pybo!'
+def index():
+    question_list = Question.query.order_by(Question.create_date.desc())
+    return render_template('question/question_list.html', question_list=question_list)
 
-@bp.route('/hello')
-def hello_world():
-    return 'Hello World~!'
+@bp.route('/detail/<int:question_id>')
+def detail(question_id):
+    question = Question.query.get(question_id)
+    return render_template('question/question_detail.html', question=question)
