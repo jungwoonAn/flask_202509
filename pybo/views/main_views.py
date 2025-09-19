738c6833bcd6f190b9492b40f224a14c5f798470
jungwoonAn <red0808@naver.com>
2025-09-19 18:19:28 +0900

    20250919

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,16 +1,8 @@
-from flask import Blueprint, render_template
-
-from pybo.models import Question
+from flask import Blueprint, redirect, url_for
 
 # 블루 프린트 : 라우팅 함수를 관리하는 역할
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    question_list = Question.query.order_by(Question.create_date.desc())
-    return render_template('question/question_list.html', question_list=question_list)
-
-@bp.route('/detail/<int:question_id>')
-def detail(question_id):
-    question = Question.query.get(question_id)
-    return render_template('question/question_detail.html', question=question)
+    return redirect(url_for('question._list'))
